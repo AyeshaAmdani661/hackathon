f1171cfbe1bf1b97d351f5db0941423798597681
AyeshaAmdani661 <ayeshaamdani661@gmail.com>
2022-08-02 21:50:16 +0500

    save changes

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,19 @@
 document.getElementById('login').addEventListener('click', function () {
     document.querySelector('.bg-modal').style.display = 'flex';
+    document.querySelector('.bg-modal .container').style.display = 'block';
     document.querySelector('body').style.overflow = 'hidden';
     document.getElementById('remove').classList.remove('fixed-top');
-
 });
 document.querySelector('.close').addEventListener('click', function () {
     document.querySelector('.bg-modal').style.display = 'none';
     document.querySelector('body').style.overflow = 'scroll';
     document.getElementById('remove').classList.add('fixed-top');
 });
-
-const container = document.querySelector('.container'),
+// document.querySelector('.new').addEventListener('click', function () {
+//     document.querySelector('.bg-modal .container').style.display = 'none';
+//     document.querySelector('.bg-modal .message').style.display = 'block';
+// });
+let container = document.querySelector('.container'),
     showHide = document.querySelectorAll('.show-hide'),
     password = document.querySelectorAll('.password'),
     signUp = document.querySelector('.signup-link'),
@@ -37,18 +40,17 @@ showHide.forEach(eyeIcon=>{
 signUp.addEventListener("click",( )=>{
     forms.style.height='540px'
     forms.style.marginLeft='-100%'
-
 });
 login.addEventListener("click",( )=>{
     forms.style.height='430px'
     forms.style.marginLeft='0%'
-
 });
-
-
-
-
-
+//  function display (a){
+//     a
+//     return a;
+//  }
+// var displayNone = document.querySelector('.bg-modal').style.display = 'none';
+// var displayresult = document.querySelector('.bg-modal').style.display = 'flex';
 
 
 
