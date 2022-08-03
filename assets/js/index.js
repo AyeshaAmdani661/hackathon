34df351565b965c8a6558a0cb63baeedad92460a
AyeshaAmdani661 <ayeshaamdani661@gmail.com>
2022-08-03 23:43:26 +0500

    save changes

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,49 +1,113 @@
-document.getElementById('login').addEventListener('click', function () {
-    document.querySelector('.bg-modal').style.display = 'flex';
-    document.querySelector('.bg-modal .container').style.display = 'block';
+scrollBody = () => {
+    document.querySelector('body').style.overflow = 'scroll';
+}
+hiddenBody = () => {
     document.querySelector('body').style.overflow = 'hidden';
+}
+addFixedtopClass = () => {
+    document.getElementById('remove').classList.add('fixed-top');
+}
+removeFixedtopClass = () => {
     document.getElementById('remove').classList.remove('fixed-top');
+}
+showBgmodal = () => {
+    document.querySelector('.bg-modal').style.display = 'flex';
+}
+hideBgmodal = () => {
+    document.querySelector('.bg-modal').style.display = 'none';
+}
+changeBackgoundOfBgmodal = () => {
+    document.querySelector('.bg-modal').style.background = 'rgba(0, 0, 0, 0.3)';
+}
+changeBackgoundOfBgmodalAgain = () => {
+    document.querySelector('.bg-modal').style.background = 'rgba(0, 0, 0, 0.7)';
+}
+showContainerOfBgmodal = () => {
+    document.querySelector('.bg-modal .container').style.display = 'block';
+}
+hideContainerOfBgmodal = () => {
+    document.querySelector('.bg-modal .container').style.display = 'none';
+}
+showLoginMessageOfBgmodal = () => {
+    document.querySelector('.bg-modal .login-message').style.display = 'block';
+}
+hideLoginMessageOfBgmodal = () => {
+    document.querySelector('.bg-modal .login-message').style.display = 'none';
+    hideBgmodal();
+    scrollBody();
+    addFixedtopClass();
+}
+showSignupMessageOfBgmodal = () => {
+    document.querySelector('.bg-modal .signup-message').style.display = 'block';
+}
+hideSignupMessageOfBgmodal = () => {
+    document.querySelector('.bg-modal .signup-message').style.display = 'none';
+    hideBgmodal();
+    scrollBody();
+    addFixedtopClass();
+}
+loginStyle = () => {
+    forms.style.height = '430px'
+    forms.style.marginLeft = '0%'
+}
+close = () => {
+    hideBgmodal();
+    scrollBody();
+    addFixedtopClass();
+    loginStyle();
+}
+document.getElementById('login').addEventListener('click', function () {
+    showBgmodal();
+    showContainerOfBgmodal();
+    hiddenBody();
+    removeFixedtopClass();
 });
 document.querySelector('.close').addEventListener('click', function () {
-    document.querySelector('.bg-modal').style.display = 'none';
-    document.querySelector('body').style.overflow = 'scroll';
-    document.getElementById('remove').classList.add('fixed-top');
+    close();
 });
-// document.querySelector('.new').addEventListener('click', function () {
-//     document.querySelector('.bg-modal .container').style.display = 'none';
-//     document.querySelector('.bg-modal .message').style.display = 'block';
-// });
+document.querySelector('.login-button').addEventListener('click', function () {
+    showBgmodal();
+    hideContainerOfBgmodal();
+    changeBackgoundOfBgmodal();
+    showLoginMessageOfBgmodal();
+});
+document.querySelector('.signup-button').addEventListener('click', function () {
+    showBgmodal();
+    hideContainerOfBgmodal();
+    changeBackgoundOfBgmodal();
+    showSignupMessageOfBgmodal();
+});
+
 let container = document.querySelector('.container'),
     showHide = document.querySelectorAll('.show-hide'),
     password = document.querySelectorAll('.password'),
     signUp = document.querySelector('.signup-link'),
     login = document.querySelector('.login-link'),
-    forms =document.querySelector('.forms');
+    forms = document.querySelector('.forms');
 
-showHide.forEach(eyeIcon=>{
-    eyeIcon.addEventListener("click",()=>{
-        password.forEach(password =>{
-            if(password.type === 'password'){
+showHide.forEach(eyeIcon => {
+    eyeIcon.addEventListener("click", () => {
+        password.forEach(password => {
+            if (password.type === 'password') {
                 password.type = 'text';
-                showHide.forEach(icon =>{
-                    icon.classList.replace("fa-eye-slash","fa-eye")
+                showHide.forEach(icon => {
+                    icon.classList.replace("fa-eye-slash", "fa-eye")
                 })
-            }else{
+            } else {
                 password.type = 'password';
-                showHide.forEach(icon =>{
-                    icon.classList.replace("fa-eye","fa-eye-slash")
+                showHide.forEach(icon => {
+                    icon.classList.replace("fa-eye", "fa-eye-slash")
                 })
             }
         })
     })
 })
-signUp.addEventListener("click",( )=>{
-    forms.style.height='540px'
-    forms.style.marginLeft='-100%'
+signUp.addEventListener("click", () => {
+    forms.style.height = '540px'
+    forms.style.marginLeft = '-100%'
 });
-login.addEventListener("click",( )=>{
-    forms.style.height='430px'
-    forms.style.marginLeft='0%'
+login.addEventListener("click", () => {
+loginStyle();
 });
 //  function display (a){
 //     a
